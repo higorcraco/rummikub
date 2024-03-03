5f43dfe848e6da7a2e361a775b3617d2cfaa35b1
higorcraco <higorcraco@gmail.com>
2024-03-03 20:00:45 -0300

    adicionando drag drop

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TileComponent } from './tile/tile.component';
+import { TileComponent } from './components/tile/tile.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TileComponent],
+  imports: [CommonModule, RouterOutlet, DragDropModule, TileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
